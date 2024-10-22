ee87b796d19b4563d1fb977e7804efadfa05af37
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-10-22 18:45:49 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCoreMvc2.Introduction.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AspNetCoreMvc2.Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to new AspNet Core Application";
+        }
+
+        public ViewResult Index2()
+        {
+            return View();
+        }
+        public ViewResult Index3()
+        {
+            List<Employee> emloyees = new List<Employee>
+            {
+                new Employee{Id=1, FirstName="Barış" , LastName="Bükümcüler" , CityId=6},
+                new Employee{Id=2, FirstName="Onur" , LastName="Bükümcüler" , CityId=36},
+                new Employee{Id=3, FirstName="Ali" , LastName="Bükümcüler" , CityId=75}
+            };
+            List<string> cities = new List<string> { "İstanbul", "Bursa" };
+
+            var emloyeeListViewModel = new EmployeeListViewModel
+            {
+                Emloyees = emloyees,
+                cities=cities
+            };
+
+            return View(emloyeeListViewModel);
+        }
+    }
+}
---
+++ Models/EmployeeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreMvc2.Introduction.Entities;
+using System.Collections.Generic;
+
+namespace AspNetCoreMvc2.Introduction
+{
+    public class EmployeeListViewModel
+    {
+        public List<Employee> Emloyees { get; set; }
+        public List<string> cities { get; set; }
+    }
+}
