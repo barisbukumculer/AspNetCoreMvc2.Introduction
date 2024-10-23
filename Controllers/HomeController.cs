7f4c407d8d05b3f1d4238215a52f60e10fda5608
Barış Bükümcüler <barisbukumculer@gmail.com>
2024-10-23 18:12:10 +0300

    .

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using AspNetCoreMvc2.Introduction.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AspNetCoreMvc2.Introduction.Controllers
 {
@@ -15,23 +17,175 @@ namespace AspNetCoreMvc2.Introduction.Controllers
         {
             return View();
         }
+
         public ViewResult Index3()
         {
-            List<Employee> emloyees = new List<Employee>
+            List<Employee> employees = new List<Employee>
             {
-                new Employee{Id=1, FirstName="Barış" , LastName="Bükümcüler" , CityId=6},
-                new Employee{Id=2, FirstName="Onur" , LastName="Bükümcüler" , CityId=36},
-                new Employee{Id=3, FirstName="Ali" , LastName="Bükümcüler" , CityId=75}
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Barış",
+                    LastName = "Bükümcüler",
+                    CityId = 6,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Onur",
+                    LastName = "Bükümcüler",
+                    CityId = 36,
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Ali",
+                    LastName = "Bükümcüler",
+                    CityId = 75,
+                },
             };
             List<string> cities = new List<string> { "İstanbul", "Bursa" };
 
             var emloyeeListViewModel = new EmployeeListViewModel
             {
-                Emloyees = emloyees,
-                cities=cities
+                Emloyees = employees,
+                cities = cities,
             };
 
             return View(emloyeeListViewModel);
+        }
+
+        public StatusCodeResult Index4()
+        {
+            //return StatusCode(200);
+            return Ok();
+
+        }
+
+        public StatusCodeResult Index5()
+        {
+            //return StatusCode(400);
+            return BadRequest();
+        }
+        public RedirectResult Index6()
+        {
+            return Redirect("/Home/Index3");
+        }
+        public IActionResult Index7()
+        {
+            return RedirectToAction("Index2");
+        }
+        public IActionResult Index8()
+        {
+            return RedirectToRoute("default");
+        }
+        public JsonResult Index9()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Barış",
+                    LastName = "Bükümcüler",
+                    CityId = 6,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Onur",
+                    LastName = "Bükümcüler",
+                    CityId = 36,
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Ali",
+                    LastName = "Bükümcüler",
+                    CityId = 75,
+                },
+            };
+            return Json(employees);
+        }
+        public IActionResult RazorDemo()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Barış",
+                    LastName = "Bükümcüler",
+                    CityId = 6,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Onur",
+                    LastName = "Bükümcüler",
+                    CityId = 36,
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Ali",
+                    LastName = "Bükümcüler",
+                    CityId = 75,
+                },
+            };
+            List<string> cities = new List<string> { "İstanbul", "Bursa" };
+
+            var emloyeeListViewModel = new EmployeeListViewModel
+            {
+                Emloyees = employees,
+                cities = cities,
+            };
+
+            return View(emloyeeListViewModel);
+        }
+
+        public JsonResult Index10(string key)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FirstName = "Barış",
+                    LastName = "Bükümcüler",
+                    CityId = 6,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FirstName = "Onur",
+                    LastName = "Bükümcüler",
+                    CityId = 36,
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FirstName = "Ali",
+                    LastName = "Bükümcüler",
+                    CityId = 75,
+                },
+            };
+            if (String.IsNullOrEmpty(key))
+            {
+                return Json(employees);
+            }
+            var result = employees.Where(e => e.FirstName.ToLower().Contains(key));
+
+            return Json(result);
+        }
+        public ViewResult EmployeeForm()
+        {
+            return View();
+        }
+
+        public string RouteData(int id)
+        {
+            return id.ToString();
         }
     }
 }
--- Entities/Employee.cs
+++ Entities/Employee.cs
@@ -1,8 +1,11 @@
-﻿namespace AspNetCoreMvc2.Introduction.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreMvc2.Introduction.Entities
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int CityId { get; set; }
